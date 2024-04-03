2766858f84df90ffe3517b5916a89df75b3248a1
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-04-03 18:03:36 +0900

    add comment

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
+      // ダークモードの設定
       darkTheme: ThemeData(
         colorSchemeSeed: Colors.deepPurple,
         brightness: Brightness.dark,
